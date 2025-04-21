975f030cc3d0f28d3440df4f253753da7f4fc53f
anton <anton_zv04@mail.ru>
2025-04-21 13:59:45 +0300

    add: dockerfile for project

--- bread_project/bread/models.py
+++ bread_project/bread/models.py
@@ -9,24 +9,6 @@ class IntItem(models.Model):
         abstract = True
 
 
-class User(models.Model):
-    UserID           = models.CharField(primary_key=True, max_length=50)
-    Name             = models.CharField(max_length=100)
-    Surname          = models.CharField(max_length=100)
-    Nickname         = models.CharField(max_length=100)
-    Create_Date      = models.DateTimeField(auto_now_add=True)
-    Last_Redact_Date = models.DateTimeField(auto_now=True)
-    email            = models.EmailField(unique=True)
-    CommentIDs       = models.ArrayField(
-        model_container=IntItem,
-        default=list
-                       )
-    hash_Password    = models.CharField(max_length=255)
-
-    class Meta:
-        db_table = 'User'
-
-
 class Recipe(models.Model):
     RecipeID   = models.CharField(primary_key=True, max_length=50)
     Title      = models.CharField(max_length=200)
@@ -43,14 +25,3 @@ class Recipe(models.Model):
         db_table = 'Recipe'
 
 
-class Comment(models.Model):
-    CommentID = models.CharField(primary_key=True, max_length=50)
-    Username  = models.CharField(max_length=100)
-    Text      = models.TextField()
-    Rating    = models.IntegerField()
-    Date      = models.DateTimeField(auto_now_add=True)
-    RecipeID  = models.CharField(max_length=50)
-    Title     = models.CharField(max_length=200)
-
-    class Meta:
-        db_table = 'Comment'
--- bread_project/user_profile/models.py
+++ bread_project/user_profile/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+
+class IntItem(models.Model):
+    value = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class User(models.Model):
+    UserID           = models.CharField(primary_key=True, max_length=50)
+    Name             = models.CharField(max_length=100)
+    Surname          = models.CharField(max_length=100)
+    Nickname         = models.CharField(max_length=100)
+    Create_Date      = models.DateTimeField(auto_now_add=True)
+    Last_Redact_Date = models.DateTimeField(auto_now=True)
+    email            = models.EmailField(unique=True)
+    CommentIDs       = models.ArrayField(
+        model_container=IntItem,
+        default=list
+                       )
+    hash_Password    = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'User'
+
+class Comment(models.Model):
+    CommentID = models.CharField(primary_key=True, max_length=50)
+    Username  = models.CharField(max_length=100)
+    Text      = models.TextField()
+    Rating    = models.IntegerField()
+    Date      = models.DateTimeField(auto_now_add=True)
+    RecipeID  = models.CharField(max_length=50)
+    Title     = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'Comment'
