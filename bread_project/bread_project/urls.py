7553eb62b2d2ad1d5a6c7dc19abf297f3d8002ae
Evgeniy Mordasov <dragotank@yandex.ru>
2025-04-24 23:31:56 +0300

    fix path again

--- bread_project/bread_project/urls.py
+++ bread_project/bread_project/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("bread/", include("bread.urls")),
     path("", include("bread.urls")),
     path("profile/", include("user_profile.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
