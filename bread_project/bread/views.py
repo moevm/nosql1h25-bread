26fbc1d293a08fa8b7568c78185984b88112f844
SunchesSun <anton_zv04@mail.ru>
2025-05-29 01:24:55 +0300

    add: search

--- bread_project/bread/views.py
+++ bread_project/bread/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe
+from django.db.models import Q
 
 # Create your views here.
 
 def bread_list(request):
     template = "bread/bread_list.html"
-    breads = Recipe.objects.all()
-    context = {"breads": breads}
+    query = request.GET.get('q', '')
+    if query:
+        breads = Recipe.objects.filter(
+            Q(Title__icontains=query) | Q(Composition__icontains=query)
+        )
+    else:
+        breads = Recipe.objects.all()
+    context = {"breads": breads, "query": query}
     return render(request, template_name=template, context=context)
 
 
